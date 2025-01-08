8ccdd598ed4c19b50022a06bf4801a41a1062c50
Amy Liao <amzhliao@gmail.com>
2025-01-07 21:07:42 -0600

    updated and added waitlist info

--- src/_components/ui/banner.tsx
+++ src/_components/ui/banner.tsx
@@ -8,7 +8,7 @@ const DiscoverProgramBanner = () => {
       <Alert className="cursor-pointer hover:bg-gray-50 bg-white shadow-sm border-[#40B4B4]">
         <AlertCircle className="h-4 w-4 text-[#40B4B4]" />
         <AlertTitle className="text-[rgb(64,180,180)] font-extrabold">
-          Discover Program Application Now Available!!!
+          The Discover Program application is now closed, but you can still apply to join the waitlist.
         </AlertTitle>
         <AlertDescription className="text-gray-600">
           Click to learn more.
