06bc1e68a3237d2a7e9443f75dab8ad8408448e8
bbrockbrown <brockbrown46@gmail.com>
2025-11-19 12:58:50 -0600

    change banner wording

--- src/_components/ui/banner.tsx
+++ src/_components/ui/banner.tsx
@@ -8,10 +8,10 @@ const DiscoverProgramBanner = () => {
       <Alert className="cursor-pointer hover:bg-gray-50 bg-white shadow-sm border-[#40B4B4]">
         <AlertCircle className="h-4 w-4 text-[#40B4B4]" />
         <AlertTitle className="text-[rgb(64,180,180)] font-extrabold">
-          Discover Program Application Now Available!!!
+          Discover Program Applications release November 24th!
         </AlertTitle>
         <AlertDescription className="text-gray-600">
-          Join our exciting discovery program and unlock new opportunities.
+          View what our exciting discovery program looks like and unlock new opportunities.
           Click to learn more.
         </AlertDescription>
       </Alert>
